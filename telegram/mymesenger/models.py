7f85d652fb660f2e45b3c78c659fece35aea847e
Роман <romankits08@gmail.com>
2025-10-04 19:16:29 +0300

    початок розробки моделей

--- telegram/mymesenger/models.py
+++ telegram/mymesenger/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Profile(models.Model):
+    icon = models.ImageField(upload_to='icons/', null=True, blank=True, default='default_icons/default.png')
+    name = models.CharField(max_length = 100, null=False, blank=False)
+    lastname = models.CharField(max_length = 100, null=True, blank=True)
+    username = models.CharField(max_length = 100, null=False, blank=False, unique=True)
+    bio = models.CharField(max_length = 500, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    slug = AutoSlugField(populate_from='username', unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Chat(models.Model):
+    users = models.ManyToManyField(Profile, related_name='chats', blank=False)
+    
+
+class Message(models.Model):
+    pass
+
+class Attachment(models.Model):
+    pass
+
+class Reaction(models.Model):
+    pass
